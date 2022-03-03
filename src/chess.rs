ddf69514db7e524261e5cc1a7d1705473b7e372f
Benjamin Graham <benwilliamgraham@gmail.com>
2022-03-03 01:58:09 -0500

    Added interfacing

--- src/chess.rs
+++ src/chess.rs
@@ -18,15 +18,16 @@ pub enum Type {
     King = 5,
 }
 
+#[derive(Clone, Copy)]
 pub struct Piece {
     data: u8,
 }
 
+// pieces are serialized as u8
+// 0b11111111 for empty
+// 0b000MCTTT where M is if the piece has moved, C is color, T is type
 impl Piece {
-    // pieces are serialized as u8
-    // 0b11111111 for empty
-    // 0b000MCTTT where M is if the piece has moved, C is color, T is type
-    const EMPTY: Piece = Piece { data: 0b11111111 };
+    pub const EMPTY: Piece = Piece { data: 0b11111111 };
 
     pub fn new(color: Color, type_: Type, has_moved: bool) -> Piece {
         let mut data = 0;
@@ -35,6 +36,44 @@ impl Piece {
         data |= (has_moved as u8) << 4;
         Piece { data }
     }
+
+    pub fn color(&self) -> Color {
+        if self.data & 0b00001000 == 0 {
+            Color::Black
+        } else {
+            Color::White
+        }
+    }
+
+    pub fn type_(&self) -> Type {
+        let data = self.data & 0b00000111;
+        match data {
+            0 => Type::Pawn,
+            1 => Type::Rook,
+            2 => Type::Knight,
+            3 => Type::Bishop,
+            4 => Type::Queen,
+            5 => Type::King,
+            _ => panic!("Invalid piece type"),
+        }
+    }
+
+    pub fn has_moved(&self) -> bool {
+        self.data & 0b00010000 != 0
+    }
 }
 
 pub type Board = [[Piece; 8]; 8];
+
+pub fn get_legal_moves(board: Board, turn: Color) -> Vec<Board> {
+    let mut legal_moves = Vec::new();
+    legal_moves
+}
+
+pub fn get_best_move(board: Board, turn: Color) -> Board {
+    board
+}
+
+pub fn get_state(board: Board) -> GameState {
+    GameState::Playing
+}
--- src/lib.rs
+++ src/lib.rs
@@ -16,21 +16,57 @@ impl FromString for chess::Color {
     }
 }
 
+impl FromString for chess::Board {
+    fn from_string(s: &str) -> Self {
+        let mut board = [[chess::Piece::EMPTY; 8]; 8];
+        board
+    }
+}
+
 pub trait ToString {
     fn to_string(&self) -> String;
 }
 
+impl ToString for chess::Board {
+    fn to_string (&self) -> String {
+        let mut s = String::new();
+        s
+    }
+}
+
+impl ToString for Vec<chess::Board> {
+    fn to_string(&self) -> String {
+        let mut s = String::new();
+        s
+    }
+}
+
+impl ToString for chess::GameState {
+    fn to_string(&self) -> String {
+        let mut s = String::new();
+        s
+    }
+}
+
 #[wasm_bindgen]
 pub fn get_legal_moves(board: &str, turn: &str) -> String {
-    "Moves".into()
+    chess::get_legal_moves(
+        chess::Board::from_string(board),
+        chess::Color::from_string(turn),
+    )
+    .to_string()
 }
 
 #[wasm_bindgen]
 pub fn get_best_move(board: &str, turn: &str) -> String {
-    "Best Move".into()
+    chess::get_best_move(
+        chess::Board::from_string(board),
+        chess::Color::from_string(turn),
+    )
+    .to_string()
 }
 
 #[wasm_bindgen]
 pub fn get_state(board: &str) -> String {
-    "Playing".into()
+    chess::get_state(chess::Board::from_string(board)).to_string()
 }
